703b0ed4d0b67e78bb42ad3d6ebd10cee404554b
Fatimat <fatimat@Abzs-MacBook-Air.local>
2025-07-15 23:39:11 +0100

    sample secrets via github action

--- src/Features/Landing/index.tsx
+++ src/Features/Landing/index.tsx
@@ -1,8 +1,10 @@
-const Landing = () => (
-    <>
+const Landing = () => {
+    console.log("example secret", import.meta.env.VITE_SAMPLE_SECRET)
+    return (<>
         <h1>aso wa</h1>
     </>
-)
+    )
+}
 
 
 export default Landing
