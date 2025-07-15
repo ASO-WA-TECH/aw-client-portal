d45f53833685d79ad8cfb6f4aa0ba344c89e655c
Fatimat <fatimat@Abzs-MacBook-Air.local>
2025-07-15 23:11:07 +0100

    fix build via github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig, type UserConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, type UserConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/aw-client-portal",
   plugins: [react()],
-  base: "/",
   test: {
     // 👋 add the line below to add jsdom to vite
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
-    setupFiles: "./tests/setup.ts", // assuming the test folder is in the root of our project
-  },
-} as UserConfig);
+    setupFiles: './tests/setup.ts', // assuming the test folder is in the root of our project
+  }
+} as UserConfig)
