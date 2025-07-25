458237f309456d54bddfcbf975abbfb8208fa0bb
Fatimat <fatimat@Abzs-MacBook-Air.local>
2025-07-25 15:23:52 +0100

    #12 Button component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,8 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
+    '@storybook/addon-interactions'
   ],
   "framework": {
     "name": "@storybook/react-vite",
---
+++ src/stories/Button/index.tsx
@@ -0,0 +1,42 @@
+import './index.scss';
+
+export interface ButtonProps {
+  /** Button contents */
+  text: string;
+  /** Click handler */
+  handleClick: () => void;
+  /** Optional disabled state - defaults to false*/
+  isDisabled?: boolean;
+  /** Optional tyoe state - defaults to button */
+  type?: 'button' | 'submit';
+  /** Which colour to use */
+  color?: 'emeraldGreen' | 'mustardYellow' | 'cream' | 'pink';
+  /** Button action type control */
+  variant?: 'primary' | 'secondary' | 'tertiary';
+}
+
+/** UI component for user interaction */
+const Button = ({
+  text,
+  handleClick,
+  isDisabled = false,
+  color = 'emeraldGreen',
+  variant = 'primary',
+  type = 'button',
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={['button', `button--${color}--${variant}`, `button--${variant}`, isDisabled ? `button--isDisabled` : ''].join(' ')}
+      onClick={handleClick}
+      disabled={isDisabled}
+      data-testid='button'
+      {...props}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button
