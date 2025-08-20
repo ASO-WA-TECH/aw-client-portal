15ad10b459ada3d9ce917d7eafc6725f7a429ec6
Fatimat <fatimat@Abzs-MacBook-Air.local>
2025-08-20 22:55:57 +0100

    #21 bottom navigation bar

--- src/stories/Button/index.tsx
+++ src/stories/Button/index.tsx
@@ -13,8 +13,6 @@ export interface ButtonProps {
   color?: 'emeraldGreen' | 'mustardYellow' | 'cream' | 'pink';
   /** Button action type control */
   variant?: 'primary' | 'secondary' | 'tertiary';
-  /** Button custom styling */
-  customStyle?: string
 }
 
 /** UI component for user interaction */
@@ -25,13 +23,12 @@ const Button = ({
   color = 'emeraldGreen',
   variant = 'primary',
   type = 'button',
-  customStyle = '',
   ...props
 }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={['button', `button--${color}--${variant}`, `button--${variant}`, isDisabled ? `button--isDisabled` : '', customStyle].join(' ')}
+      className={['button', `button--${color}--${variant}`, `button--${variant}`, isDisabled ? `button--isDisabled` : ''].join(' ')}
       onClick={handleClick}
       disabled={isDisabled}
       data-testid='button'
