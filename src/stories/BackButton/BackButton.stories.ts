e60fd61134385a4b74f986e8de91f5e25bedc071
bukixo <bukithompson@gmail.com>
2026-05-11 18:48:41 +0100

    Updated backbutton

--- src/stories/BackButton/BackButton.stories.ts
+++ src/stories/BackButton/BackButton.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { BackButton } from "./BackButton";
+import BackButton from "./BackButton";
 
 const meta: Meta<typeof BackButton> = {
   title: "Components/BackButton",
